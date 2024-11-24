3c3ceb48a5cc501db8d264ca05bd6bfda36ab7ec
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2024-11-24 15:54:42 -0300

    Criando e Estruturando a API
    Criando e Estruturando a API com GET e Banco de Dados

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 import express from "express";
 
+const posts = [
+    { id: 1, descricao: "Uma foto linda de um gatinho", imagem: "https://placecats.com/millie/300/150" },
+    { id: 2, descricao: "Paisagem deslumbrante de um pôr do sol", imagem: "https://placecats.com/millie/300/150" },
+    { id: 3, descricao: "Mais uma Paisagem", imagem: "https://placecats.com/millie/300/150" },
+];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Boas vindas à imersão! A Torre Eiffel iluminada a noite, com milhares de luzes cintilando, criando um espetáculo mágico em Paris!");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+
+function buscarPostPorId(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id)
+    })
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorId(req.params.id)
+    res.status(200).json(posts[index]);
 });
